48f1365ff28aa27c8ab058da9f47b52037cd743a
Anders Kleimark <akleimark@gmail.com>
2022-09-01 16:18:28 +0200

    Jag lade till några konstanter.

--- include/Defs.h
+++ include/Defs.h
@@ -3,6 +3,7 @@
 
 #include <wx/string.h>
 
+// ID som används
 const wxWindowID MAIN_FRAME_ID = -1;
 const unsigned int ID_LIST_CHESSPLAYERS = 1;
 const unsigned int ID_RESET_DATABASE = 2;
@@ -14,11 +15,17 @@ const unsigned int ID_LIST_TOURNAMENT_PLAYERS = 5;
 const unsigned int XML_BUTTON_ID = 0;
 const unsigned int TEXTFILE_BUTTON_ID = 1;
 
-const wxString APPLICATION_NAME = "ChessPairer";
+// Heltalskonstanter
 const unsigned int MINIMUM_LENGTH = 2;
-const unsigned int MINIMUM_YEAR = 1900;
 const unsigned int DEFAULT_FRAME_WIDTH = 700;
 const unsigned int DEFAULT_FRAME_HEIGHT = 700;
+
+// Textkonstanter
+const wxString QUIT_MESSAGE = "Avsluta";
+const wxString ABOUT_MESSAGE = "Om";
+const wxString APPLICATION_NAME = "ChessPairer";
+const wxString DEVELOPER_NAME = "Anders Kleimark";
+const wxString DEVELOPER_EMAIL = "akleimark@gmail.com";
 const wxString GENERAL_ERROR_MESSAGE = "Fel";
 const wxString ADD_STRING = L"L\u00E4gg till";
 const wxString EDIT_STRING = L"\u00C4ndra";
@@ -29,7 +36,6 @@ const wxString FORM_ERROR = L"Det finns fel i formul\u00E4ret.";
 const wxString CONFIRM_MESSAGE = L"\u00C4r du s\u00E4ker p\u00E5 att du vill ";
 const wxString CONFIRM_LABEL = "Svara";
 const wxString SUCCESS_MESSAGE = "Klart";
-
 const wxString NO_RESULT = "-";
 
 
